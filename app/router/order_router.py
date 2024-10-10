a9d00402e3d7db2cc53b543e280005c574fe6cb7
Ayush Srivastava <Ayush.20b0121122@abes.ac.in>
2024-10-10 14:28:11 +0530

    payment gateway

--- app/router/order_router.py
+++ app/router/order_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends, Request, BackgroundTasks
-from app.crud.order_crud import create_order_logic, retrieve_order_history, get_order_by_session_id
+from app.crud.order_crud import create_order_logic, retrieve_order_history
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from app.models.order import Order
@@ -11,7 +11,6 @@ from app.utils.invoice import send_sms_notification, send_email_with_invoice, ge
 import stripe
 from app.config import settings
 from decimal import Decimal
-from fastapi.responses import JSONResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
@@ -20,46 +19,6 @@ stripe.api_key= settings.STRIPE_API_KEY
 @router.post("/")
 async def create_order(order_data: OrderCreate, background_tasks: BackgroundTasks, request: Request, current_buyer: Buyer = Depends(get_current_user)):
     order = await create_order_logic(order_data, current_buyer)
-    
-    #return order
-    
-    #order_response = OrderResponse(
-    #     order_id=str(order.id),
-    #     buyer_name=current_buyer.full_name,
-    #     items=[
-    #         OrderItem(
-    #             product_name=item.product.name,
-    #             quantity=item.quantity,
-    #             price=item.product.price,
-    #             seller=SellerInfo(
-    #                 seller_name=item.product.seller.full_name,
-    #                 seller_contact=item.product.seller.phone_no,
-    #                 store_name=item.product.seller.store_name,
-    #                 store_location=item.product.seller.store_address
-    #             )
-    #         ) for item in order.items
-    #     ],
-    #     total_price=order.total_price,
-    #     final_price=order.final_price,
-    #     coupon_code=order_data.coupon_code,  
-    #     payment_method=order_data.payment_method,
-    #     payment_status=order.payment_status,
-    #     delivery_address=DeliveryAddress(
-    #         address_line1=order.delivery_address.address_line1,
-    #         address_line2=order.delivery_address.address_line2,
-    #         city=order.delivery_address.city,
-    #         state=order.delivery_address.state,
-    #         postal_code=order.delivery_address.postal_code,
-    #         country=order.delivery_address.country
-    #     ),
-    #     order_date=order.order_at,
-    #     charge=ChargeResponse(
-    #         amount=order.charge.amount if order.charge else Decimal(0.0),
-    #         stripe_charge_id=order.charge.stripe_charge_id if order.charge else None,
-    #         created_at=order.charge.created_at if order.charge else None,
-    #     ) if order.charge else None
-    # )
-    
     
     if order_data.payment_method == PaymentMethod.COD:
         pdf_path = generate_invoice_pdf(order)
